56324cb399a92852a0591a6652685f08574b7dbd
airacalins <95123097+airacalins@users.noreply.github.com>
2026-05-18 15:36:09 +0800

    client: render the ai output

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -2,12 +2,18 @@ import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
 import axios from 'axios';
+import { useState } from 'react';
 
 type FormData = {
   prompt: string;
 };
 
+type ChatResponse = {
+  message: string;
+};
+
 const ChatBot = () => {
+  const [messages, setMessages] = useState<string[]>([]);
   const {
     register,
     handleSubmit,
@@ -16,12 +22,16 @@ const ChatBot = () => {
   } = useForm<FormData>();
 
   const onSubmit = async ({ prompt }: FormData) => {
+    setMessages((prev) => [...prev, prompt]);
+
     reset();
 
-    const { data } = await axios.post('/api/chat', {
+    const { data } = await axios.post<ChatResponse>('/api/chat', {
       prompt,
       conversationId: crypto.randomUUID(),
     });
+
+    setMessages((prev) => [...prev, data.message]);
 
     console.log(data);
   };
@@ -34,24 +44,31 @@ const ChatBot = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      onKeyDown={onKeyDown}
-      className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
-    >
-      <textarea
-        {...register('prompt', {
-          required: true,
-          validate: (data) => data.trim().length > 0,
-        })}
-        className="w-full focus:outline-0 resize-none"
-        placeholder="Ask anything"
-        maxLength={1000}
-      />
-      <Button disabled={!isValid} className="rounded-full w-9 h-9">
-        <FaArrowUp />
-      </Button>
-    </form>
+    <div>
+      <div>
+        {messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        onKeyDown={onKeyDown}
+        className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+      >
+        <textarea
+          {...register('prompt', {
+            required: true,
+            validate: (data) => data.trim().length > 0,
+          })}
+          className="w-full focus:outline-0 resize-none"
+          placeholder="Ask anything"
+          maxLength={1000}
+        />
+        <Button disabled={!isValid} className="rounded-full w-9 h-9">
+          <FaArrowUp />
+        </Button>
+      </form>
+    </div>
   );
 };
 
