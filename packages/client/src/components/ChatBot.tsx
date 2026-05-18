bb57d1792d521085dec2f7a13aacdb54f4da97f7
airacalins <95123097+airacalins@users.noreply.github.com>
2026-05-18 15:14:18 +0800

    client: add react hook form

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,18 +1,51 @@
+import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
 
+type FormData = {
+  prompt: string;
+};
+
 const ChatBot = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid },
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+
+    reset();
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(onSubmit)();
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      onKeyDown={onKeyDown}
+      className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+    >
       <textarea
+        {...register('prompt', {
+          required: true,
+          validate: (data) => data.trim().length > 0,
+        })}
         className="w-full focus:outline-0 resize-none"
         placeholder="Ask anything"
         maxLength={1000}
       />
-      <Button className="rounded-full w-9 h-9">
+      <Button disabled={!isValid} className="rounded-full w-9 h-9">
         <FaArrowUp />
       </Button>
-    </div>
+    </form>
   );
 };
 
