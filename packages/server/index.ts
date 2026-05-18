920b94f7e9291e01e7d447de4e797990fbfa1a2d
airacalins <95123097+airacalins@users.noreply.github.com>
2026-05-18 13:54:37 +0800

    server: create conversation repository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,6 +3,7 @@ import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
 import z from 'zod';
+import { conversationRepository } from './repositories/conversation.repository';
 
 dotenv.config();
 
@@ -22,8 +23,6 @@ app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello from the API!' });
 });
 
-const conversations = new Map<string, string>();
-
 const chatSchema = z.object({
   prompt: z
     .string()
@@ -34,25 +33,32 @@ const chatSchema = z.object({
 });
 
 app.post('/api/chat', async (req: Request, res: Response) => {
-  const parseResult = chatSchema.safeParse(req.body);
+  try {
+    const parseResult = chatSchema.safeParse(req.body);
 
-  if (!parseResult.success) {
-    res.status(400).json(parseResult.error.format());
+    if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+    }
+
+    const { prompt, conversationId } = req.body;
+
+    const response = await client.responses.create({
+      model: 'gpt-4o-mini!',
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 100,
+      previous_response_id:
+        conversationRepository.getLastResponseId(conversationId),
+    });
+
+    conversationRepository.setLastResponseId(conversationId, response.id);
+
+    res.json({ message: response.output_text });
+  } catch (error) {
+    res.status(500).json({
+      error: 'Failed to generate a response.',
+    });
   }
-
-  const { prompt, conversationId } = req.body;
-
-  const response = await client.responses.create({
-    model: 'gpt-4o-mini',
-    input: prompt,
-    temperature: 0.2,
-    max_output_tokens: 100,
-    previous_response_id: conversations.get(conversationId),
-  });
-
-  conversations.set(conversationId, response.id);
-
-  res.json({ message: response.output_text });
 });
 
 app.listen(port, () => {
