a48069bb566fac55d7c48c0f2c7f294971f30176
airacalins <95123097+airacalins@users.noreply.github.com>
2026-05-18 14:21:25 +0800

    server: create route module

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,24 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { chatService } from './services/chat.service';
-import { chatController } from './controllers/chat.controller';
-import type { Request, Response } from 'express';
+import router from './routes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.json({ message: 'Hello from the API!' });
-});
-
-app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
