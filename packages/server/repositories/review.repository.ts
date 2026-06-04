7bd4daea4352aba024b5e949df50d0839081d25b
airacalins <95123097+airacalins@users.noreply.github.com>
2026-06-04 09:07:35 +0800

    server: return summary if not yet expired

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -33,4 +33,12 @@ export const reviewRepository = {
       update: data,
     });
   },
+
+  getReviewSummary(productId: number) {
+    return prisma.summary.findUnique({
+      where: {
+        productId,
+      },
+    });
+  },
 };
